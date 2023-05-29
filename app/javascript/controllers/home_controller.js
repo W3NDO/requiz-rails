131fea72e7d40d19525ce5c57de27f9d15fd2f23
w3ndo <wendonyang@gmail.com>
2023-05-29 21:19:55 +0300

    feat(develop-pw/quiz) - changes in CSS for responsiveness

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -4,4 +4,34 @@ export default class extends ApplicationController {
   connect () {
     super.connect()
   }
+
+  static targets = ["dropdown", "toggleBtn", "selectionBar"]
+
+  toggleDropdown(){
+    if (Array.from(this.dropdownTarget.classList).includes("hidden")){
+      this.dropdownTarget.classList.remove("hidden")
+      this.toggleBtnTarget.innerHTML = `<span class="text-3xl"> <i class="fa-solid fa-xmark" aria-hidden="true"></i> </span>`
+    } else {
+      this.dropdownTarget.classList.add("hidden")
+      this.toggleBtnTarget.innerHTML = `<span class="text-3xl"> <i class="fa-solid fa-bars" aria-hidden="true"></i> </span> `
+    }
+  }
+
+  optionSelected(){
+    this.toggleDropdown();
+  }
+
+  toggleSelectionBar(){
+    var arr = Array.from(this.selectionBarTarget.classList)
+    if(!(arr.includes("not-mobile"))){ //because tailwind is mobile first, this action will only happen on screens smaller than the lg breakpoint
+      if(arr.includes("hidden")){
+        this.selectionBarTarget.classList.remove("hidden")
+      }else{
+        this.selectionBarTarget.classList.add("hidden")      
+      }
+    }
+  }
 }
+
+
+            
