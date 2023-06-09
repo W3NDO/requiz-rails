3c93a63a459ffb9fc6debe4ebda7dfac373fdbf9
w3ndo <wendonyang@gmail.com>
2023-06-09 09:55:55 +0300

    chore(develop-pw/api) - routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'docs/index'
+  
   # resources :questions
-  resources :quizzes
-  get '/kshn', to: "kshn#index"
   # resources :notes
-  resource :example, constraints: -> { Rails.env.development? }
+  resources :quizzes
+  
+  get '/kshn', to: "kshn#index"
+  get 'docs/index'
   get 'home/index'
+  
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
   root "home#index"
 end
