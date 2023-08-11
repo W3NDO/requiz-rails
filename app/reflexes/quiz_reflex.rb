821b200af7485ed957e2ddb907cecf1bf5c8d3e3
W3NDO <wendonyang@gmail.com>
2023-08-11 18:07:42 +0300

    feat(develop-pw/ui_redesign) - the take quiz screens now working - after a quiz is complete, it updates the score on the db

--- app/reflexes/quiz_reflex.rb
+++ app/reflexes/quiz_reflex.rb
@@ -13,17 +13,17 @@ class QuizReflex < ApplicationReflex
   end
 
   def start_quiz
-    # TODO sounds??
-    cable_ready.add_css_class(selector: "#start_btn", name: "is-disabled")
-    cable_ready.set_attribute(selector: "#start_btn", name: "disabled", value: true)
-    cable_ready.remove_css_class(selector: "#start_btn", name: "is-primary")
+    # TODO :: add sounds??
+    # cable_ready.add_css_class(selector: "#start_btn", name: "is-disabled")
+    # cable_ready.set_attribute(selector: "#start_btn", name: "disabled", value: true)
+    # cable_ready.remove_css_class(selector: "#start_btn", name: "is-primary")
 
     questions = Quiz.includes(:questions).find(element.dataset[:id]).questions
     question_count = questions.length
     correct_answer_count = 0
     next_index = 1
     my_answers = []
-    morph "#quiz_main", render(partial: "quizzes/question", locals: {
+    morph "#main_container", render(partial: "quizzes/question", locals: {
       :question => questions.first, 
       :question_count => question_count,
       :correct_answer_count => correct_answer_count,
@@ -38,7 +38,8 @@ class QuizReflex < ApplicationReflex
   def check_answer
     has_next = element.dataset["order"].split(",").last != element.dataset["qid"]
     question = Question.find_by(id: element.dataset["qid"])
-    questions = question.quiz.questions
+    quiz = question.quiz
+    questions = quiz.questions
     order = element.dataset["order"]
     next_index = element.dataset["next_index"].to_i
     
@@ -55,7 +56,7 @@ class QuizReflex < ApplicationReflex
       cable_ready.remove_css_class(selector: "#start_btn", name: "is-primary")
       next_question = questions[next_index]
       next_index += 1
-      morph "#quiz_main", render(partial: "quizzes/question", locals: {
+      morph "#main_container", render(partial: "quizzes/question", locals: {
         :question => next_question, 
         :question_count => questions.length,
         :correct_answer_count => correct_answer_count,
@@ -68,8 +69,11 @@ class QuizReflex < ApplicationReflex
       cable_ready.add_css_class(selector: "#start_btn", name: "is-primary")
       cable_ready.remove_attribute(selector: "#start_btn", name: "disabled")
       cable_ready.remove_css_class(selector: "#start_btn", name: "is-disabled")
+      score = ((correct_answer_count.to_f/questions.length.to_f)*100).round(2)
+      quiz.update!(score: score) # Update quize with the last score they got
 
-      morph "#quiz_main", render(partial: "quizzes/end_of_quiz", locals: {
+      morph "#main_container", render(partial: "quizzes/end_of_quiz", locals: {
+        :quiz => question.quiz,
         :question_count => questions.length,
         :correct_answer_count => correct_answer_count,
       })
